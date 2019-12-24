6b1a9274b7bb4d8365ccecbdb723a85b309164c2
zxx <mhxy514zxx@163.com>
2019-12-24 17:01:10 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+    <!-- 公共底部 -->
+    <FooterGuide></FooterGuide>
+  </div>
+</template>
+
+<script>
+import FooterGuide from '@/components/FooterGuide/FooterGuide.vue'
+export default {
+  name: 'App',
+  components: {
+    FooterGuide
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/FooterGuide/FooterGuide.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="footer_guide">
+    底部
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FooterGuide',
+  methods: {
+
+  }
+}
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+@import '../../common/stylus/mixins.styl';
+
+.footer_guide { // footer
+  top-border-1px(#e4e4e4);
+  position: fixed;
+  z-index: 100;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #fff;
+  width: 100%;
+  height: 50px;
+  display: flex;
+
+  .guide_item {
+    display: flex;
+    flex: 1;
+    text-align: center;
+    flex-direction: column;
+    align-items: center;
+    margin: 5px;
+    color: #999999;
+
+    &.on {
+      color: #02a774;
+    }
+
+    span {
+      font-size: 12px;
+      margin-top: 2px;
+      margin-bottom: 2px;
+
+      .iconfont {
+        font-size: 22px;
+      }
+    }
+  }
+}
+</style>
